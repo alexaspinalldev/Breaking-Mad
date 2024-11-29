66b78607256b04e91ba3904f1b5ba87aaece1bd3
Somerset Dromgoole <samersetredram@gmail.com>
2024-11-29 13:41:49 +0000

    updated my branch code to reflect the main branch, updated our images to contain more landscapes  positioned our imageBox and changed the quote background to help legibility

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,25 +25,14 @@ async function newQuote() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // my array of images
 const images = [
     'assets/images/bg images/Forest.webp',
-    'assets/images/bg images/Sunset.jpg',
-    'assets/images/bg images/Swingset.jpg',
+    'assets/images/bg images/Landscape Sunset.webp',
+    'assets/images/bg images/Swingset landscape.jpg',
     'assets/images/bg images/Underwater Scene.webp',
     'assets/images/bg images/Waterfall.webp',
+    'assets/images/bg images/Mountain Range.jpg'
 
 ];
 
@@ -52,8 +41,68 @@ const newBgBtn = document.getElementById('newBgBtn'); // This gets the button
 
 // Function to update the background image
 function newBg() {
+function newBg() {
     currentIndex = (currentIndex + 1) % images.length; // Loop back to 0 after the last image so that we cycle through all images
     imageBox.style.backgroundImage = `url('${images[currentIndex]}')`; // Updates the image
     console.log(`Background changed to: ${images[currentIndex]} (Index: ${currentIndex})`); // Logs the change on the console so I can see what I'm doing
+    randomPosish() // Randomises the postion of the quote
 }
-newBgBtn.addEventListener('click', newBg); // waits for the click
+// newBgBtn.addEventListener('click', changeBackground); // No need for this as were using onclick - Alex
+
+
+
+/// Randomised layout of the quote
+var removePosition = [];
+
+function randomPosish() {
+    let random3 = () => Math.floor(Math.random() * 3); // Get a random number 0-2
+
+    let positionList = [];
+
+    if (removePosition.length > 0) { // If there are positioning classes to remove
+        for (let position of removePosition) { // loop through the array
+        imageBox.classList.remove(position); // and remove each class
+        console.log("Removed class:", position);}
+    } else {console.log("false")}
+    console.log("Removed array:", removePosition);
+
+
+    // Randomly assign a new alignment class
+    switch (random3()) {
+        case 0:
+            align = "align-items-start";
+            break;
+        case 1:
+            align = "align-items-center";
+            break;
+        case 2:
+            align = "align-items-end";
+            break;
+    }
+
+    // Randomly assign a new justification class
+    switch (random3()) {
+        case 0:
+            justify = ["justify-content-start", "text-start"];
+            break;
+        case 1:
+            justify = ["justify-content-center", "text-center"];
+            break;
+        case 2:
+            justify = ["justify-content-end", "text-end"];
+            break;
+    }
+
+    //Assemble the positionList array here
+    positionList.push(align, ...justify)
+
+    removePosition = positionList; // Update the array to remove when the function re-runs
+
+    // Add the new classes to imageBox
+    for (let position of positionList) { // Loop through the array
+        imageBox.classList.add(position); // and add each class
+        console.log("Added class:", position);}
+    console.log("Added array:", positionList);
+}
+
+
